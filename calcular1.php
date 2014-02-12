f352a708f6f30be78d3caa625b234b0b445ee1c5
Daniel <dmartingar@uoc.edu>
2014-02-12 20:05:57 +0100

    Create calcular1.php
    Ejercicio de calculadora sencilla en el que utilizamos el archivo calcula.html junto con este php. Nos permite realizar algunas operaciones básicas y hemos añadido la opción de que la calculadora nos muestre el resultado de la división sin decimales.

---
+++ calcular1.php
@@ -0,0 +1,27 @@
+<?php
+if ($_POST ["valor1"] !="" and $_POST ["valor2"]!=""){
+if ($_POST["operador"] == "suma") {
+print ($resultado = $_POST ["valor1"] + $_POST ["valor2"]);
+print ('<br /><a href="calcula1.html">Volver</a>');
+}
+elseif ($_POST["operador"] == "resta") {
+print ($resultado = $_POST ["valor1"] - $_POST ["valor2"]);
+print ('<br /><a href="calcula1.html">Volver</a>');
+}
+elseif ($_POST["operador"] == "multiplica") {
+print ($resultado = $_POST ["valor1"] * $_POST ["valor2"]);
+print ('<br /><a href="calcula1.html">Volver</a>');
+}
+elseif ($_POST["operador"] == "divide") {
+print ($resultado = $_POST ["valor1"] / $_POST ["valor2"]);
+print ('<br /><a href="calcula1.html">Volver</a>');
+}
+elseif ($_POST["operador"] == "dividSinDecimal") {
+print ($resultado = ((int)($_POST ["valor1"] / $_POST ["valor2"])));
+print ('<br /><a href="calcula1.html">Volver</a>');
+}
+else {
+print("Dame un valor");
+print ('<br /><a href="calcula1.html">Volver</a>');
+}
+}
