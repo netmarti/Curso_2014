260045c5bde181398378d85bbaa35bb37274e7ca
Daniel <dmartingar@uoc.edu>
2014-02-14 13:01:15 +0100

    Update diagrama01.js
    Para evitar error al lanzar el navegador hemos eliminado la función que habíamos definido pero que no estamos utilizando y además hemos modificado el texto de la segunda alerta que se va a mostrar por pantalla (puedes indicar el mensaje que quieras).

--- diagrama01.js
+++ diagrama01.js
@@ -2,14 +2,14 @@
  * @author Daniel
  */
 
-		function diagrama01(){var_1;
-    	num_1=window.prompt("Dame un valor")
-    	if(num_1>30){
-    		alert(num_1+"Es mayor a 30");
-    	}else
-    	{
-    		alert(num_1+"Es inferior a 30");
-    	}
-    	}
+function diagrama01() { 
+	
+	num_1 = window.prompt("Dame un valor")
+	if (num_1 > 30) {
+		alert(num_1 + "Es mayor a 30");
+	} else {
+		alert(num_1 + "Es inferior a 30");
+	}
+}
     	
     	
