17ee44eefedb5e13fd7d01c003b22dd93bc563b6
Daniel <dmartingar@uoc.edu>
2014-02-14 12:41:13 +0100

    Create diagrama01.js
    Creamos una función que llamaremos function01 y que vamos a llamar desde el diagrama01.html para ello hemos de crear el botón en el archivo htm.

---
+++ diagrama01.js
@@ -0,0 +1,15 @@
+/**
+ * @author Daniel
+ */
+
+		function diagrama01(){var_1;
+    	num_1=window.prompt("Dame un valor")
+    	if(num_1>30){
+    		alert(num_1+"Es mayor a 30");
+    	}else
+    	{
+    		alert(num_1+"Es inferior a 30");
+    	}
+    	}
+    	
+    	
