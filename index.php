fc0758d3c8f29f74dea8fd65214597dd11962f75
Daniel <dmartingar@uoc.edu>
2014-02-21 01:48:18 +0100

    Create index.php
    Preparamos el documento para interactuar con las BBDD

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//creamos la sesion
+session_start();
+//validamos si se ha hecho o no el inicio de sesion correctamente
+//si no se ha hecho la sesion nos regresará a login.php
+if(!isset($_SESSION['usuario'])) 
+{
+  header('Location: login.php'); 
+  exit();
+}
+ ?>
+  <h1>BIENVENIDO</h1>
+  <a href="logout.php">Cerrar Sesión</a>
+ <?
+?>
