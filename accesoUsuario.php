f0acbb16f92be6b20fd68ebbc8329d4236361796
Daniel <dmartingar@uoc.edu>
2014-02-21 01:44:06 +0100

    Create accesoUsuario.php
    Preparamos un archivo para nuestro ejercicio en el que empezamos a interactuar con BBDD

---
+++ accesoUsuario.php
@@ -0,0 +1,17 @@
+<?
+<form action="validar_usuario.php" method="post">
+ <table>
+  <tr>
+   <td>Usuario:</td>
+   <td><input name="admin" required="required" type="text" /></td>
+  </tr>
+  <tr>
+   <td>Password:</td>
+   <td><input name="password_usuario" required="required" type="password" /></td> 
+  </tr>
+  <tr>
+   <td colspan="2"><input name="iniciar" type="submit" value="Iniciar Sesión" /></td>
+  </tr>
+</table>
+</form>
+?>
