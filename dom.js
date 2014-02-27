82a7a40cc53612ea4f3e2d790b4af0dfcab07134
Daniel <dmartingar@uoc.edu>
2014-02-27 13:43:39 +0100

    Create dom.js
    Práctica propuesta para familiarizarnos con el DOM. Utilizaremos también los documentos dom.css y dom.js para ejecutar la práctica 20. Dejamos el documento preparado para ir resolviendo los ejercicios propuestos en esta práctica.

---
+++ dom.js
@@ -0,0 +1,11 @@
+/**
+ * @author Daniel
+ */
+
+function ej01(){
+	var texto = window.prompt("Introduce un texto"); //el usuario nos pasa el texto desde el div
+	
+	var elemDIV = document.getElementById('addText'); //hemos guardado el elemento que contiene addText
+	
+	
+}
