e22431159591be112f11d3488ee2d85adcea3d70
Daniel <dmartingar@uoc.edu>
2014-02-28 11:34:25 +0100

    Update dom.js
    Añadimos las funciones para interactuar con el usuario, de modo que al clicar sobre los dos primeros botones nos realicen determinada acción, funciones ej01 y ej02.

--- dom.js
+++ dom.js
@@ -11,6 +11,29 @@ function ej01(){
 	
 	}//cierra la funcion ej01()
 	
-// cuando se clike sobre SET COLOR 
+	
+	/**Cuando clickemos setColor pide un color al usario y lo coloca en el background del botón**/
+	
+function ej02(){
+	
+	var cambiaColor = window.prompt("Dime un color");// el usuario nos pasa un nuevo color
+	
+	var elemCol = document.getElementById('setColor');// guardamos el elemento que contiene setColor
+	
+	elemCol.style.backgroundColor = cambiaColor;
+	
+	if(color == "blue"){
+				elemCol.style.backgroundColor="red";
+			}
+				else{
+				elemCol.style.backgroundColor="blue";
+			}//cierra el else
+
+}//cierra la funcion ej02()
+			
+	
+	
+
+
 	
 
