0c207ed9fbaa78d25890c093470442dd52c73a3a
Daniel <dmartingar@uoc.edu>
2014-03-02 14:21:56 +0100

    Create controlEstructuras.js
    Ejercicio para familiarizarnos con las estructuras de control en JS

---
+++ controlEstructuras.js
@@ -0,0 +1,15 @@
+<script type=”text/javascript”>
+var manos = 12;      
+var pies = 12;
+manos= window.prompt(“Número de dedos en las manos”);
+pies= window.prompt(“Número de dedos en los pies”);        
+var manos= window.parseInt(manos);                       //Pasa manos a un valor del tipo Number
+var pies= window.parseInt(pies);                               //Pasa manos a un valor del tipo Number
+var resultado;
+var resultado = (manos+pies);                                   //Realizamos la operación
+
+  if (resultado > 20){
+	alert(“Tienes polidactilia. Como Halle Berry, Frank Tolbert Pranamya Menaria…”);
+  }
+
+</script>
