31f72895124b188a8d922e6da3b4f4069ae207fe
Daniel <dmartingar@uoc.edu>
2014-03-02 18:03:55 +0100

    Create switchActrices.js
    Estructuras de control y swtich

---
+++ switchActrices.js
@@ -0,0 +1,17 @@
+<script type=“text/javascript”>
+var actriz = window.prompt(“¿Cuál de ellas es mejor actriz?\K.Z Jones, Demi Moore o Kameron Díaz”);
+swithc (actriz)
+{
+	 case “K.Z Jones”
+alert(“El Oscar es para K.Z Jones”);
+break;
+case “Kameron Días”
+alert(“El Oscar es para Kameron Díaz”);
+break;
+case “Demi Moore”
+alert(“El Oscar es para Demi Moore”);
+break;
+defalult:
+	alert(“El Oscar quedó desierto”);
+}     //cierra el switch
+</script>
