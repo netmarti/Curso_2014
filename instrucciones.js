70c09540d766d49ccc68fca83c76dcaf94e77212
Daniel <dmartingar@uoc.edu>
2014-03-05 18:35:14 +0100

    Create instrucciones.js
    Algunos ejemplos para afianzar conceptos

---
+++ instrucciones.js
@@ -0,0 +1,15 @@
+//La instrucción RETURN (palabra reservada en JS)
+
+<script type=”text/javascript”
+
+    var palabra =””;
+    while (palabra !=”fin”)
+    {
+      palabra = prompt(“Dime la palabra clave”);
+      if(palabra == “fin”)
+      {
+        alert("Has escrito la palabra mágica");
+        break;
+      } 
+}
+</script>
