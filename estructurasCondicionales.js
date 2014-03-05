7001ff7982a20a6a3a2756f6a07be44c8308f7df
Daniel <dmartingar@uoc.edu>
2014-03-05 20:15:30 +0100

    Create estructurasCondicionales.js

---
+++ estructurasCondicionales.js
@@ -0,0 +1,76 @@
+// Sentencia if
+
+<script type=”text/javascript”>
+
+var num1, num2;
+
+if (num1 > num2){
+			document.write(“num1 es mayor que num2”)
+}
+
+</script>
+
+// BUCLE if ... else
+
+<script type=”text/javascript”>
+
+function saludoUser(){
+		var nombre;
+		if(nombre==”Pepe”)
+{
+			(“Buenos días Pepe”);
+}//cierra el if
+else{
+	alert(“Si no eres Pepe contacta con el administrador del sistema”);
+}//cierra el else
+
+}//cierra la función saludo
+
+</script>
+
+/* BUCLE for, utilizado sobre todo como contador, repitiéndose un número determinado de veces
+ repite un trozo de código cuando la condición es TRUE
+ Tiene las siguientes partes definidas
+ Inicialización, condición, ejecución y actualización
+
+
+<script type=”text/javascript”>
+
+    for (valorInicial, condicion, actualizacion)
+    {
+      instrucciones a ejecutar en el bucle
+      
+    }//cierra el for
+<script> 
+
+*/
+
+<script type=”text/javascript”>
+
+    var contador=0:
+    
+    for (contador, contador<11, contador++)
+    {
+      document.write("Este es el número "+ contador);
+      
+    }//cierra el for
+<script>
+
+// estructura de control if...else
+
+<script type=”text/javascript”>
+
+function saludoUser(){
+		var nombre;
+		if(nombre==”Pepe”)
+{
+			(“Buenos días Pepe”);
+}//cierra el if
+else{
+	alert(“Si no eres Pepe contacta con el administrador del sistema”);
+}//cierra el else
+
+}//cierra la función saludo
+
+</script>
+
