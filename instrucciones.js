ce9efeef71e333c01905dac3d985354fce857c0e
Daniel <dmartingar@uoc.edu>
2014-03-05 18:58:36 +0100

    Update instrucciones.js
    Cuando necesitamos salir de un bucle o detener la ejecución del programa podemos utilizar diversas expresiones en función de la necesidad de nuestro código.

--- instrucciones.js
+++ instrucciones.js
@@ -1,5 +1,26 @@
 //La instrucción RETURN (palabra reservada en JS)
 
+<script type=”text/javascript” />
+
+    function multipleReturn(num){
+
+                                    var resto = num%2
+
+                                    if(resto == 0){
+							                        return 0;
+                                                    }//cierra el if
+                                    else
+                                        {
+	                                    return num;
+                                        }//cierra el else
+
+                                    }//cierra la función
+
+
+</script>
+
+//La instrucción BREAK;
+
 <script type=”text/javascript”
 
     var palabra =””;
@@ -13,3 +34,21 @@
       } 
 }
 </script>
+
+// La instrucción CONTINUE;
+
+<script type="text/javascript">
+
+        var num = 0;
+        
+        for (num=0;num<100;num++){
+            
+            if (num/2 == parseInt(num/2))
+            {
+                continue
+            }//cierra el if
+            document.write("El número"+num+"<br/>");
+        }//cierra el for
+</script>
+
+
