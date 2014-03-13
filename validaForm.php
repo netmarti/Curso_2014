fc90d9b67dbe0489b2d610a3977531b35193e0bb
Daniel <dmartingar@uoc.edu>
2014-03-13 11:31:14 +0100

    Create validaForm.php
    Empezamos a preparar el documento para las validaciones, práctica 21

---
+++ validaForm.php
@@ -0,0 +1,15 @@
+<?php
+
+$nombre = $_GET['nombre'];
+
+@header("Content-type: text/xml");
+$xml = '<?xml version="1.0" encoding="utf-8"?>'. "\n";
+
+if (strlen($nombre)>2 && strlen($nombre)<15){
+	$xml.='<info>'."\n";
+	$xml.='<valor><![CDATA['.$nombre.']]></valor>'."\n";
+	
+	
+}//cierra el if
+
+?>
