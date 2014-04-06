327d98dfd0accc5a7deedb24ca72ab6ab8a7348d
Daniel <dmartingar@uoc.edu>
2014-04-06 11:15:40 +0200

    Create Operadores.java
    Documento básico para irnos familiarizando con el uso de operadores en la estructura de un programa escrito en Java. Podemos reutilizar el código cambiando el signo del operador. Hemos declarado la variable como un float para que al realizar divisiones sean más precisas, con la variable declarada como int se pierde precisión.

---
+++ Operadores.java
@@ -0,0 +1,11 @@
+class Operadores{
+/**
+ *
+ * @author dan
+ */
+public static void main(String [] args) {
+   float altura = 123; //colocamos float porque al dividir si es int no es preciso
+   altura = altura % 7;// cambiamos el operador para las diferentes operaciones
+   System.out.println(altura);
+}
+}
