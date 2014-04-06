7eb44cbc02e4f61dd84006585e543918471455dd
Daniel <dmartingar@uoc.edu>
2014-04-06 11:22:00 +0200

    Create VariosOperadores.java
    Seguimos familiarizándonos con el trabajo de varios operadores en la estructura del programa.

---
+++ VariosOperadores.java
@@ -0,0 +1,15 @@
+class Operadores{
+/**
+ *
+ * @author dan
+ */
+ 
+public static void main(String [] args) {
+
+   int puntuacion = 12; //definimos la variable con la que vamos a operar
+   int total = 333+(puntuacion*10);
+   
+   //Solicitamos que se imprima por pantalla el resultado almacenado en la variable total
+   System.out.println(total);
+  }
+}
