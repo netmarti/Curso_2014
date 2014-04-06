21810d37413c36789d64345fa392002721391d29
Daniel <dmartingar@uoc.edu>
2014-04-06 12:12:18 +0200

    Create Prioridad2.java
    Prioridad 2:
    Se mantiene el valor de la variable x a la que no se realiza el incremento hasta que se ha completado la operación que tiene a la derecha.

---
+++ Prioridad2.java
@@ -0,0 +1,10 @@
+class Prioridad2 {
+    public static void main(String [] args){
+        int x=9;
+        float z = x++ * 3 + 2 * 5 / 2;
+        System.out.println(z);
+        System.out.println(x);
+    
+    }
+    
+}
