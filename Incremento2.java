ed6b17a9bd2110d1eb6946399b61f63242a5ba0d
Daniel <dmartingar@uoc.edu>
2014-04-06 11:34:07 +0200

    Create Incremento2.java
    Variable con el aumento como prefijo. Es decir primero aumentamos el valor y luego realizamos la operación matemática.

---
+++ Incremento2.java
@@ -0,0 +1,12 @@
+class Incremento {
+/**
+ *
+ * @author dan
+ */
+public static void main(String [] args) {
+   int x=3;
+   int valor = ++x *10;
+   System.out.println(valor);
+   System.out.println(x);
+}
+}
