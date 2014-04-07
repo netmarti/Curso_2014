f92a2e6f6a842a00ae99176016137fcb450f7067
Daniel <dmartingar@uoc.edu>
2014-04-07 10:18:45 +0200

    Create proces14a.java
    Práctica 14A

---
+++ proces14a.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Daniel
+ */
+public class proces14a extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String nota = request.getParameter("nota");
+        
+        String mensaje = " ";
+        int notaInt = Integer.parseInt(nota);
+        
+        switch (notaInt){
+            case 10:
+                mensaje = "Matrícula de Honor";
+                break;
+            case 9:
+                mensaje = "Sobresaliente";
+                break;
+            case 8: case 7:
+                mensaje = "Notable";
+                break;
+            case 6: case 5:
+                mensaje = "Aprobado";
+                break;
+            case 4: case 3: case 2: case 1: case 0:
+                mensaje = "Pide ayuda a tu profesor. Has suspendido";
+                break;
+                
+            default:
+                mensaje = "Nota mal ingresada";
+                break;       
+        }
+        
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Práctica 14 A</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h2>Calificaciones</h2>");
+            out.println("<p> "+mensaje+"</p>");
+            out.println("<a href=\"index.jsp\"><button>Volver</button></a>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
