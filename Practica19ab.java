f6fabbe4e5af0b55a12f7976a6d6e929f69e3675
Daniel <dmartingar@uoc.edu>
2014-04-07 23:09:20 +0200

    Create Practica19ab.java
    Práctica 19 cálculos con interés compuesto

---
+++ Practica19ab.java
@@ -0,0 +1,25 @@
+/*
+ * Debe llamarse UsaDeitel_5_14
+ */
+
+package controller;
+
+/**
+ *
+ * @author dan
+ */
+public class UsaDeitel_5_14
+
+  {              // Abre clase UsaDeitel_5_14
+ 
+  public static void main(String args[])
+  {       // Abre main 
+  Deitel_5_14 miObjeto = new Deitel_5_14();
+
+  // Llama metodo UsaDeitel_5_14
+
+  miObjeto.Interes(); 
+
+  }        // Cierra main
+
+  }              // Cierra clase UsaDeitel_5_14
