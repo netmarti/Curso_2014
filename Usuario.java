91d2479faae3b3099dec0d81a7c1398d8a5da615
Daniel <dmartingar@uoc.edu>
2014-04-11 01:10:26 +0200

    Create Usuario.java
    Para ejercicio de Autentificación con login

---
+++ Usuario.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fjsp.modelo;
+
+/**
+ *
+ * @author dan
+ */
+public class Usuario {
+    private String username ="";
+    private String nombre ="";
+    private String password ="";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
