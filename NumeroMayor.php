3b8efaccdc98d28f07039b0e8fbe676d3f5f92ee
Daniel <dmartingar@uoc.edu>
2014-04-28 23:05:57 +0200

    Create NumeroMayor.php
    Ejercicios de repaso PHP

---
+++ NumeroMayor.php
@@ -0,0 +1,17 @@
+<?php
+//declaramos las variables
+$num1= 2;
+$num2= 24;
+
+//Usamos el bucle if else para
+//encontrar el mayor de los dos valores
+
+if ($num1>$num2){
+
+	print "El valor: ".$num1." es mayor que el valor: ".$num2;
+
+}else{
+	print "El valor: ".$num2." es mayor que el valor: ".$num1;
+}
+
+?>
