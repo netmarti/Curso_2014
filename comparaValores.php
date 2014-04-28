1ff834f27df99bdfec347a10573a9e07b0452d8c
Daniel <dmartingar@uoc.edu>
2014-04-28 22:24:50 +0200

    Create comparaValores.php
    Ejercicios de repaso PHP: comparaValores.php

---
+++ comparaValores.php
@@ -0,0 +1,20 @@
+<?php
+//declaramos las variables
+$num1= 2;
+$num2= 2;
+
+//Usamos el bucle if elseif else para
+//encontrar el mayor de los dos valores o comprobar la igualdad
+
+if ($num1>$num2){
+
+	print "El valor: ".$num1." es mayor que el valor: ".$num2;}
+
+elseif ($num1==$num2){
+	print"Los valores ".$num1." y ".$num2." son iguales.";}
+		
+else{print "El valor: ".$num2." es mayor que el valor: ".$num1;}
+
+
+
+?>
