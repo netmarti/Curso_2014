0af440686dcb173036be3a46400f5da1afea2568
Daniel <dmartingar@uoc.edu>
2014-04-28 22:23:44 +0200

    Create DoWhile.php
    Ejercicios de repaso PHP: DoWhile.php

---
+++ DoWhile.php
@@ -0,0 +1,15 @@
+<?php
+//Do While (se ejecuta al menos una vez)
+// La condición se comprueba al principio de cada iteracción.
+// Si esta se evalúa como false desde el principio, finaliza el bucle.
+
+$i=0;
+
+do{
+	print $i;
+
+}while ( $i>=10); 
+
+//muestra por pantalla el valor del contador $i, aquí vale 0
+
+?>
