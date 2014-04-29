15f8694475e920977c67c101cf5e02a3cc2b26ac
Daniel <dmartingar@uoc.edu>
2014-04-29 23:29:01 +0200

    Update and rename inicioDeSesion.php to inicioDeSesionyDesconexion.php

--- inicioDeSesionyDesconexion.php
+++ inicioDeSesionyDesconexion.php
@@ -1,10 +1,14 @@
-<html>
 <?php
 session_start();
 //iniciamos el sistema de sesiones, no podemos dejar ni un espacio delante. Cargamos la variable de sesion 
 
 $_SESSION['nombre'] = $_POST['nombre'];
 $_SESSION['clave'] = $_POST['clave'];
+
+//finalizamos la sesión
+session_start();
+session_unset();//finalizamos desconectando la sesión
+session_destroy();//borra la sesión completamente, si queremos acceder debemos volver a crearla
 
 ?>
 
