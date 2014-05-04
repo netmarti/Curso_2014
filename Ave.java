c48c9714c43d49580b6bf61c450e85a0e94d28fe
Daniel <dmartingar@uoc.edu>
2014-05-04 17:53:37 +0200

    Create Ave.java
    Tienda de mascotas, clase hija de mascota

---
+++ Ave.java
@@ -0,0 +1,42 @@
+package beans;
+
+/**
+ *
+ * @author dan
+ */
+public class Ave extends Mascota {
+    
+    private String comportamiento;
+
+    /**
+     *
+     * @param chip
+     * @param tipoMascota
+     * @param nomMascota
+     * @param alimentacion
+     * @param habitat
+     * @param sangre
+     * @param listaMascotas
+     * @param ArrayList
+     * @param comportamiento // diurno o nocturno
+     */
+    public Ave(String chip, String tipoMascota, String nomMascota, String alimentacion, String habitat, String sangre, java.util.ArrayList<Mascota> listaMascotas, Mascota ArrayList, String comportamiento) {
+        super(chip, tipoMascota, nomMascota, alimentacion, habitat, sangre, listaMascotas, ArrayList);
+        this.comportamiento = comportamiento;
+    }
+
+    public String getComportamiento() {
+        return comportamiento;
+    }
+
+    public void setComportamiento(String comportamiento) {
+        this.comportamiento = comportamiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Ave{" + "comportamiento=" + comportamiento + '}';
+    }
+
+   
+}//cierra la clase, útlima llave del documento
