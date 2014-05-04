088777b0bae0afed44c33908fbbb70c84ae1db19
Daniel <dmartingar@uoc.edu>
2014-05-04 17:51:17 +0200

    Create Reptil.java
    Tienda Mascotas, clase hija de Mascota

---
+++ Reptil.java
@@ -0,0 +1,53 @@
+package beans;
+
+/**
+ *
+ * @author dan
+ */
+public class Reptil extends Mascota {
+    
+    private String movimiento;
+    private boolean caparazon;
+
+    /**
+     *
+     * @param chip
+     * @param tipoMascota
+     * @param nomMascota
+     * @param alimentacion
+     * @param habitat
+     * @param sangre
+     * @param listaMascotas
+     * @param ArrayList
+     * @param movimiento //nada, repta, camina
+     * @param caparazon //si o no
+     */
+    public Reptil(String chip, String tipoMascota, String nomMascota, String alimentacion, String habitat, String sangre, java.util.ArrayList<Mascota> listaMascotas, Mascota ArrayList, String movimiento, boolean caparazon) {
+        super(chip, tipoMascota, nomMascota, alimentacion, habitat, sangre, listaMascotas, ArrayList);
+        this.movimiento = movimiento;
+        this.caparazon = caparazon;
+    }
+
+    @Override
+    public String toString() {
+        return "Reptil{" + "movimiento=" + movimiento + ", caparazon=" + caparazon + '}';
+    }
+
+    public String getMovimiento() {
+        return movimiento;
+    }
+
+    public void setMovimiento(String movimiento) {
+        this.movimiento = movimiento;
+    }
+
+    public boolean isCaparazon() {
+        return caparazon;
+    }
+
+    public void setCaparazon(boolean caparazon) {
+        this.caparazon = caparazon;
+    }
+
+   
+}
