73cc24a3f60d7dd0e63167d69ae84a70c5b7d193
juanHid <joan.h.iaweb@gmail.com>
2014-05-04 20:10:58 +0200

    Update Ave.java

--- Ave.java
+++ Ave.java
@@ -1,27 +1,27 @@
 package beans;
 
 /**
- *
- * @author dan
- */
+*
+* @author dan
+*/
 public class Ave extends Mascota {
     
     private String comportamiento;
 
     /**
-     *
-     * @param chip
-     * @param tipoMascota
-     * @param nomMascota
-     * @param alimentacion
-     * @param habitat
-     * @param sangre
-     * @param listaMascotas
-     * @param ArrayList
-     * @param comportamiento // diurno o nocturno
-     */
-    public Ave(String chip, String tipoMascota, String nomMascota, String alimentacion, String habitat, String sangre, java.util.ArrayList<Mascota> listaMascotas, Mascota ArrayList, String comportamiento) {
-        super(chip, tipoMascota, nomMascota, alimentacion, habitat, sangre, listaMascotas, ArrayList);
+*
+* @param chip
+* @param tipoMascota
+* @param nomMascota
+* @param alimentacion
+* @param habitat
+* @param sangre
+* @param listaMascotas
+* @param ArrayList
+* @param comportamiento // diurno o nocturno
+*/
+    public Ave(String chip, String tipoMascota, String nomMascota, String alimentacion, String habitat, String sangre,String comportamiento) {
+        super(chip, tipoMascota, nomMascota, alimentacion, habitat, sangre);
         this.comportamiento = comportamiento;
     }
 
@@ -33,10 +33,10 @@ public class Ave extends Mascota {
         this.comportamiento = comportamiento;
     }
 
-    @Override
+  /*  @Override
     public String toString() {
         return "Ave{" + "comportamiento=" + comportamiento + '}';
     }
-
+*/
    
 }//cierra la clase, útlima llave del documento
